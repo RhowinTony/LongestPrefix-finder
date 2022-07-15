7d20908634dfb778659e626a835e89c6bad02d3b
Rhowin Tony <mail2rhowintony.j@gmail.com>
2022-07-15 17:37:24 +0530

    2nd commit

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ class Solution:
         if len(strs) == 0:
             return ""
         
+        minlen = len(strs[0])
+        for i in range(len(strs)):
+            minlen = min(len(strs[i]), minlen)
+        
+        
