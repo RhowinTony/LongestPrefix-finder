22fbaa7f74d719ad8a163fbc6bf4ad0467fc3740
Rhowin Tony <mail2rhowintony.j@gmail.com>
2022-07-15 17:58:03 +0530

    ex: commit

--- main.py
+++ main.py
@@ -8,4 +8,14 @@ class Solution:
         for i in range(len(strs)):
             minlen = min(len(strs[i]), minlen)
         
-        
+        lcp = ""
+        i = 0
+        while i < minlen:
+            char = strs[0][i]
+            for j in range(i,len(strs)):
+                if strs[j][i] != char:
+                    return lcp
+            lcp = lcp + char
+            i += 1
+
+        return lcp
