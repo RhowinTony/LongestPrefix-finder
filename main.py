48063596b0fb0dbe0e9c85bdcaa3a0e5c342f538
Rhowin Tony <mail2rhowintony.j@gmail.com>
2022-07-17 22:44:57 +0530

    1st

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-class Solution:
-    def longestcommonprefix(self, strs:list[str]) -> str:
+# class Solution:
+#     def longestcommonprefix(self, strs:list[str]) -> str:
 
-        if len(strs) == 0:
-            return ""
+#         if len(strs) == 0:
+#             return ""
         
-        minlen = len(strs[0])
-        for i in range(len(strs)):
-            minlen = min(len(strs[i]), minlen)
+#         minlen = len(strs[0])
+#         for i in range(len(strs)):
+#             minlen = min(len(strs[i]), minlen)
         
-        lcp = ""
-        i = 0
-        while i < minlen:
-            char = strs[0][i]
-            for j in range(i,len(strs)):
-                if strs[j][i] != char:
-                    return lcp
-            lcp = lcp + char
-            i += 1
+#         lcp = ""
+#         i = 0
+#         while i < minlen:
+#             char = strs[0][i]
+#             for j in range(i,len(strs)):
+#                 if strs[j][i] != char:
+#                     return lcp
+#             lcp = lcp + char
+#             i += 1
 
-        return lcp
+#         return lcp
