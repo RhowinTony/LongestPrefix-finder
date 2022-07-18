98b158d3e8fc67f434680d918bbacfc496ba826e
Rhowin Tony <mail2rhowintony.j@gmail.com>
2022-07-18 23:54:30 +0530

    added basic syntx

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 
 # Write a function to find the longest common prefix string amongst an array of strings.
-
 # If there is no common prefix, return an empty string "".
-
- 
-
 # Example 1:
-
 # Input: strs = ["flower","flow","flight"]
 # Output: "fl"
+
+# class Solution:
+#     def longestcommonprefix(self, strs:list[str]) -> str:
+
+#         if len(strs) == 0:
+#             return ""
+        
+#         minlen = len(strs[0])
+#         for i in range(len(strs)):
+#             minlen = min(len(strs[i]), minlen)
+        
+#         lcp = ""
+#         i = 0
+#         while i < minlen:
+#             char = strs[0][i]
+#             for j in range(i,len(strs)):
+#                 if strs[j][i] != char:
+#                     return lcp
+#             lcp = lcp + char
+#             i += 1
+
+#         return lcp
 
 
 class LongestPrefix:
