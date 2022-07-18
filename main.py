a6e8cebe66ac23c0cd83882c5b08b9fff45e31e0
Rhowin Tony <mail2rhowintony.j@gmail.com>
2022-07-18 23:51:08 +0530

    3rd commit

--- main.py
+++ main.py
@@ -1,24 +1,3 @@
-# class Solution:
-#     def longestcommonprefix(self, strs:list[str]) -> str:
-
-#         if len(strs) == 0:
-#             return ""
-        
-#         minlen = len(strs[0])
-#         for i in range(len(strs)):
-#             minlen = min(len(strs[i]), minlen)
-        
-#         lcp = ""
-#         i = 0
-#         while i < minlen:
-#             char = strs[0][i]
-#             for j in range(i,len(strs)):
-#                 if strs[j][i] != char:
-#                     return lcp
-#             lcp = lcp + char
-#             i += 1
-
-#         return lcp
 
 # Write a function to find the longest common prefix string amongst an array of strings.
 
