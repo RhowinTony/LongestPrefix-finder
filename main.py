6076534ce23eb76e08c6839b3ef119ce601fafbb
Rhowin Tony <mail2rhowintony.j@gmail.com>
2022-07-21 17:23:11 +0530

    last commit

--- main.py
+++ main.py
@@ -9,7 +9,7 @@
 def prefix_finder(str):
     lcp = ""
     i = 0
-    while i < minlen:
+    while i < min_len:
         char = str_list[0][i]
         for j in range(1,len(str_list)):
             if str_list[j][i] != char:
@@ -19,13 +19,13 @@ def prefix_finder(str):
     return lcp
 
 
-str = input("Enter the strings ")
+str = input("Enter the strings separated by space:")
 str_list = str.split()
 if len(str_list) == 0:
      print("None")
-minlen = (len(str_list[0]))
+min_len = (len(str_list[0]))
 for _ in range(len(str_list)):
-    minlen = min(len(str_list[_]), minlen)
+    min_len = min(len(str_list[_]), min_len)
 output = prefix_finder(str_list)
 print(f"The longest common prefix is {output}")
 
